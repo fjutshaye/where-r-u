2aee2e5a0683a0e289183150915be91874ed4e47
Jeremy <tjiang4966@gmail.com>
2018-03-09 12:13:21 -0500

    delete dist folder

--- dist/server/server.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = require("./app");
-const port = process.env.PORT || 3000;
-app_1.default.listen(port, () => {
-    console.log('Node Server Is Running On Port ' + port);
-});
-//# sourceMappingURL=server.js.map
